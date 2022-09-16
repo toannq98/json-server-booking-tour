b2fee5259ce2e947b6d2e5b7a8c5ac2e445a55e6
toannq98 <nongquoctoan.dev@gmail.com>
2022-09-16 13:43:33 +0700

    first upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// /!\ Bind the router db to the server
+server.db = router.db
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(auth)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
