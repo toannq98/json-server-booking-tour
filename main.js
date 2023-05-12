701af670331c7defcf88bebaac1061362d2a9a14
Otis <nongquoctoan.dev@gmail.com>
2023-05-12 17:19:54 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
 const server = jsonServer.create()
-const router = jsonServer.router('src/db.json')
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 // Set default middlewares (logger, static, cors and no-cache)
